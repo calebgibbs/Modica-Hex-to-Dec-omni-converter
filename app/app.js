42d5c48cbb32133a55a862fde1a51c88f48f0b5c
Caleb Gibbs <calebgibbs@me.com>
2022-10-05 20:17:10 +1300

    Filter hex values out of input text

--- app/app.js
+++ app/app.js
@@ -1,24 +1,49 @@
-const submitBtn = document.getElementById('submit-btn'); 
-const inputText = document.getElementById('input-content');  
-let errorMsg = document.getElementById('error-msg');  
+const submitBtn = document.getElementById('submit-btn')
+const inputText = document.getElementById('input-content')  
+let errorMsg = document.getElementById('error-msg')  
 
 // prevent default button click   
 submitBtn.onclick = () => { 
   //get content 
-  let content = inputText.value; 
+  let contentStr = inputText.value
   
   //validate the content  
-  errorMsg.innerHTML = ''; 
-  if(content == 0) { 
-    errorMsg.innerHTML = 'Required';
+  errorMsg.innerHTML = ''
+  if(contentStr == 0) { 
+    errorMsg.innerHTML = 'Required'
   } else { 
-    filterHex(content);
+    filterHex(contentStr)
   }
 
 }
 
-filterHex = (content) => {
-  //empty array for the hex 
-  //find hex in content 
-  //pass array to new function
+filterHex = (contentStr) => {  
+  //regex to find potential hex values 
+  const potentialHexRegex = /[0-9A-Fa-f]{8}/g
+  //regex to find confirm that hex values also include letters 
+  const hexValidateRegex =  /[A-Fa-f]/g
+  
+  //add each word in the input string to an array
+  let content = []
+  content = contentStr.split(' ')
+  
+  let hexValues = []
+  
+  //loop over content array to find hex and add value to hex array
+  content.forEach(word => {
+    if(word.match(potentialHexRegex)) { 
+      if(word.match(hexValidateRegex)) {
+        hexValues.push(word)
+      }
+    }
+  }); 
+  
+  //quick validation  
+  errorMsg.innerHTML = ''
+  if(hexValues != 0) {
+    console.log('found hex')
+  } else { 
+    errorMsg.innerHTML = 'Cannot find any hexadecimal values';
+  }
+
 }
